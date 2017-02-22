1d4609b31e31bce5a69b7fd303d0a77cedd8bb85
m-lamarre <studium.square.pantz@gmail.com>
2017-02-22 17:57:12 +0200

    updated models and models user_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+
+  validates(:username, presence: true,
+            length: { maximum: 50 },
+            uniqueness: { case_sensitive: false })
+
+  validates(:email, presence: true,
+            length: { maximum: 100 },
+            uniqueness: { case_sensitive: false }
+            format: { with: EMAIL_REGEX })
+
+  validates(:password, presence: true,
+            length: { minimum: 6 })
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: "example_user", email: "test@email.com",
+                      password: "pass123", password_confirmation: "pass123")
+  end
+
+  test "" do
+
+  end
 end
